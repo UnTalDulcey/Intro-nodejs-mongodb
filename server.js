dbd0a171254f6f442792125979cbf51d379c0685
soybackend <josealvarez712@gmail.com>
2015-03-14 10:40:03 -0500

    first route example

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 //server http
 var express = require('express');
 var logger = require('./logger');
-
+var bodyParser = require('body-parser');
 
 //creamos nuestra app y el puerto por el que escuchará
 var app = express();
 var port = 3000;
+
+//nos permite parsear las peticiones como json
+app.use(bodyParser.json('application/json'));
+
+//ruta inicial
+app.get('/', function(req, res) {
+  
+  logger.info('GET /');
+
+  res.json({
+    hola: "hola!"
+  })
+
+});
+
 
 //le decimos a nuestra app que empiece a escuchar por el puerto en la variable port
 app.listen(port, function(){
